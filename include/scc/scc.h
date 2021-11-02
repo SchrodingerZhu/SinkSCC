007521fc12f1a9477387b4fc5eb55176b5f2f55a
Schrodinger ZHU Yifan <i@zhuyi.fan>
2021-11-02 14:31:27 +0800

    parametrize
    Signed-off-by: Schrodinger ZHU Yifan <i@zhuyi.fan>

--- include/scc/scc.h
+++ include/scc/scc.h
@@ -4,86 +4,128 @@
 
 #ifndef SINSCC_SCC_H
 #define SINSCC_SCC_H
-#include <iostream>
+
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
 
 namespace scc {
-    class Graph {
-    public:
-        explicit Graph(const std::vector<std::string> &names) {
-            for (const auto &i: names) {
-                nodes.insert({i, Node{i}});
-            }
+    template<class Index, class N>
+    struct VectorType {
+        using Container = std::vector<N>;
+
+        template<class Source>
+        static N const &get(Source const &src) {
+            return src;
         }
 
-        void connect(const std::string &x, const std::string &y) {
-            nodes.at(x).edges.push_back(&nodes.at(y));
-            nodes.at(y).dual_edges.push_back(&nodes.at(x));
-        }
-
-    private:
-        struct Node {
-            std::string name;
-            std::vector<Node *> edges{};
-            std::vector<Node *> dual_edges{};
-            bool visited = false;
-
-            explicit Node(std::string name) : name(std::move(name)) {}
-
-            void dual_dfs(std::vector<Node *> &acc) {
-                if (visited) return;
-                visited = true;
-                for (auto i: dual_edges) {
-                    i->dual_dfs(acc);
-                }
-                acc.push_back(this);
+        template<class Iter>
+        static Container construct(Iter begin, Iter end) {
+            Container nodes{};
+            for (auto i = begin; i != end; i = std::next(i)) {
+                nodes.push_back(N{*i});
             }
-
-            void primal_dfs(std::vector<Node *> &acc) {
-                if (visited) return;
-                visited = true;
-                acc.push_back(this);
-                for (auto i: edges) {
-                    i->primal_dfs(acc);
-                }
-            }
-        };
-
-        std::unordered_map<std::string, Node> nodes;
-
-        std::vector<Node *> dual_dfs() {
-            std::vector<Node *> ordered_nodes{};
-            for (auto &i: nodes) {
-                i.second.dual_dfs(ordered_nodes);
-            }
-            for (auto &i: nodes) {
-                i.second.visited = false;
-            }
-            return ordered_nodes;
-        }
-
-    public:
-        std::vector<std::vector<std::string>> scc() {
-            dual_dfs();
-            std::vector<std::vector<std::string>> result;
-            auto reversed_topological_order = dual_dfs();
-            for (auto i = reversed_topological_order.rbegin(); i != reversed_topological_order.rend(); ++i) {
-                if (!(*i)->visited) {
-                    result.emplace_back();
-                    std::vector<Node *> subgraph{};
-                    (*i)->primal_dfs(subgraph);
-                    for (auto j: subgraph) {
-                        result.back().push_back(j->name);
-                    }
-                }
-            }
-            for (auto &i: nodes) {
-                i.second.visited = false;
-            }
-            return result;
+            return nodes;
         }
     };
-}
+
+    template<class Index, class N>
+    struct AssocMapType {
+        using Container = std::unordered_map<Index, N>;
+
+        template<class Source>
+        static N const &get(Source const &src) {
+            return src.second;
+        }
+
+        template<class Iter>
+        static Container construct(Iter begin, Iter end) {
+            Container nodes{};
+            for (auto i = begin; i != end; i = std::next(i)) {
+                nodes.insert({*i, N{*i}});
+            }
+            return nodes;
+        }
+    };
+};
+
+template<class Index, template<class, class> typename TypeTrait>
+class Graph {
+public:
+    template<class Iter>
+    explicit Graph(Iter begin, Iter end) : nodes(TypeImpl::construct(std::move(begin), std::move(end))) {
+
+    }
+
+    void connect(const Index &x, const Index &y) {
+        nodes[x].edges.push_back(&nodes[y]);
+        nodes[y].dual_edges.push_back(&nodes[x]);
+    }
+
+private:
+    struct Node {
+        Index name;
+        std::vector<const Node *> edges{};
+        std::vector<const Node *> dual_edges{};
+        mutable bool visited = false;
+
+        Node() {}
+
+        explicit Node(Index name) : name(std::move(name)) {}
+
+        void dual_dfs(std::vector<const Node *> &acc) const {
+            if (visited) return;
+            visited = true;
+            for (auto i: dual_edges) {
+                i->dual_dfs(acc);
+            }
+            acc.push_back(this);
+        }
+
+        void primal_dfs(std::vector<const Node *> &acc) const {
+            if (visited) return;
+            visited = true;
+            acc.push_back(this);
+            for (auto i: edges) {
+                i->primal_dfs(acc);
+            }
+        }
+    };
+
+    using TypeImpl = TypeTrait<Index, Node>;
+    typename TypeImpl::Container nodes;
+
+    std::vector<const Node *> dual_dfs() {
+        std::vector<const Node *> ordered_nodes{};
+        for (auto &i: nodes) {
+            TypeImpl::get(i).dual_dfs(ordered_nodes);
+        }
+        for (auto &i: nodes) {
+            TypeImpl::get(i).visited = false;
+        }
+        return ordered_nodes;
+    }
+
+public:
+    std::vector<std::vector<Index>> scc() {
+        dual_dfs();
+        std::vector<std::vector<Index>> result;
+        auto reversed_topological_order = dual_dfs();
+        for (auto i = reversed_topological_order.rbegin(); i != reversed_topological_order.rend(); ++i) {
+            if (!(*i)->visited) {
+                result.emplace_back();
+                std::vector<const Node *> subgraph{};
+                (*i)->primal_dfs(subgraph);
+                for (auto j: subgraph) {
+                    result.back().push_back(j->name);
+                }
+            }
+        }
+        for (auto &i: nodes) {
+            TypeImpl::get(i).visited = false;
+        }
+        return result;
+    }
+};
+
 #endif //SINSCC_SCC_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,35 @@
+#include <iostream>
 #include <scc/scc.h>
 int main() {
     using namespace scc;
-    Graph graph{{
-                        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"
-                }};
-    graph.connect("A", "B");
-    graph.connect("B", "D");
-    graph.connect("B", "E");
-    graph.connect("B", "C");
-    graph.connect("C", "F");
-    graph.connect("E", "B");
-    graph.connect("E", "F");
-    graph.connect("E", "G");
-    graph.connect("F", "C");
-    graph.connect("F", "H");
-    graph.connect("G", "H");
-    graph.connect("G", "J");
-    graph.connect("H", "K");
-    graph.connect("I", "G");
-    graph.connect("J", "I");
-    graph.connect("K", "L");
-    graph.connect("L", "J");
-    auto scc = graph.scc();
-    for (auto & i  : scc) {
-        for (auto & j : i) {
-            std::cout << j << " ";
+    {
+        std::vector<std::string> nodes{
+                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"
+        };
+        Graph<std::string, AssocMapType> graph{nodes.begin(), nodes.end()};
+        graph.connect("A", "B");
+        graph.connect("B", "D");
+        graph.connect("B", "E");
+        graph.connect("B", "C");
+        graph.connect("C", "F");
+        graph.connect("E", "B");
+        graph.connect("E", "F");
+        graph.connect("E", "G");
+        graph.connect("F", "C");
+        graph.connect("F", "H");
+        graph.connect("G", "H");
+        graph.connect("G", "J");
+        graph.connect("H", "K");
+        graph.connect("I", "G");
+        graph.connect("J", "I");
+        graph.connect("K", "L");
+        graph.connect("L", "J");
+        auto scc = graph.scc();
+        for (auto &i: scc) {
+            for (auto &j: i) {
+                std::cout << j << " ";
+            }
+            std::cout << std::endl;
         }
-        std::cout << std::endl;
     }
-    return 0;
 }
